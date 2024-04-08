8d5eef200a50bc534f57e4bd7de1ea8f9675d8bc
leandromanciagli <leandromanciagli@gmail.com>
2024-04-07 21:52:40 -0300

    Flujo de compra completado

--- src/main/java/org/example/compras/FacturarServlet.java
+++ src/main/java/org/example/compras/FacturarServlet.java
@@ -16,52 +16,57 @@ import org.example.compras.model.Product;
 public class FacturarServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out=response.getWriter();
-        out.println("<html>");
-        out.println("<style>table, th, td { border:1px solid black; min-width:200px; }</style>");
-        out.println("<head><title>Carrito de compras</title></head>");
-        out.println("<body>");
-        out.println("<h1>Carrito de compras</h1>");
-        out.println("<table style='width:30%'>");
-        out.println("<tr><th>Nombre</th><th>Precio unitario</th><th>Cantidad a comprar</th><th>Precio total</th></tr>");
-        Hashtable<String, Product> products = (Hashtable<String, Product>)getServletContext().getAttribute("products");
-        double totalGeneral = 0.0;
-        HttpSession session = request.getSession();
-        Hashtable<String, Product> userProducts = (Hashtable<String, Product>)session.getAttribute("userProducts");
-        Enumeration<String> parameters = request.getParameterNames();
-        while (parameters.hasMoreElements()) {
-            String productName = parameters.nextElement();
-            double productPrice = products.get(productName).getPrice();
-            int productAmount = Integer.parseInt(request.getParameter(productName));
-            if (productAmount > 0) {
-                double productTotalPrice = productPrice * productAmount;
-                totalGeneral = totalGeneral + productTotalPrice;
-                if (userProducts.containsKey(productName)) {
-                    userProducts.get(productName).setAmount(productAmount);
-                    userProducts.get(productName).setTotalPrice(productTotalPrice);
-                } else {
-                    Product product = new Product(productName, productPrice, productAmount, productTotalPrice);
-                    userProducts.put(productName, product);
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.sendRedirect("index.jsp");
+        } else {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<style>table, th, td { border:1px solid black; min-width:200px; }</style>");
+            out.println("<head><title>Carrito de compras</title></head>");
+            out.println("<body>");
+            out.println("<h1>Carrito de compras</h1>");
+            out.println("<table style='width:30%'>");
+            out.println("<tr><th>Nombre</th><th>Precio unitario</th><th>Cantidad a comprar</th><th>Precio total</th></tr>");
+            Hashtable<String, Product> products = (Hashtable<String, Product>) getServletContext().getAttribute("products");
+            double totalGeneral = 0.0;
+            session = request.getSession();
+            Hashtable<String, Product> userProducts = (Hashtable<String, Product>) session.getAttribute("userProducts");
+            Enumeration<String> parameters = request.getParameterNames();
+            while (parameters.hasMoreElements()) {
+                String productName = parameters.nextElement();
+                double productPrice = products.get(productName).getPrice();
+                int productAmount = Integer.parseInt(request.getParameter(productName));
+                if (productAmount > 0) {
+                    double productTotalPrice = productPrice * productAmount;
+                    totalGeneral = totalGeneral + productTotalPrice;
+                    if (userProducts.containsKey(productName)) {
+                        userProducts.get(productName).setAmount(productAmount);
+                        userProducts.get(productName).setTotalPrice(productTotalPrice);
+                    } else {
+                        Product product = new Product(productName, productPrice, productAmount, productTotalPrice);
+                        userProducts.put(productName, product);
+                    }
+                    out.println("<tr>" +
+                            "<td>" + userProducts.get(productName).getName() + "</td>" +
+                            "<td><center>$ " + userProducts.get(productName).getPrice() + "</center></td>" +
+                            "<td><center>" + userProducts.get(productName).getAmount() + " U.</center></td>" +
+                            "<td><center>$ " + userProducts.get(productName).getTotalPrice() + "</center></td>" +
+                            "</tr>");
+                } else if (userProducts.containsKey(productName)) {
+                    userProducts.remove(productName);
+                    products.get(productName).setAmount(0);
                 }
-                out.println("<tr>" +
-                        "<td>" + userProducts.get(productName).getName() + "</td>" +
-                        "<td><center>$ " + userProducts.get(productName).getPrice() + "</center></td>" +
-                        "<td><center>" + userProducts.get(productName).getAmount() + " U.</center></td>" +
-                        "<td><center>$ " + userProducts.get(productName).getTotalPrice() + "</center></td>" +
-                        "</tr>");
-            } else if (userProducts.containsKey(productName)) {
-                userProducts.remove(productName);
-                products.get(productName).setAmount(0);
-            }
 
+            }
+            out.println("<tr><td>Total general</td><td></td><td></td><td><center>$ " + totalGeneral + "</center></td></tr>");
+            out.println("</table>");
+            out.println("<a href='productos'>Seguir comprando</a>");
+            out.println("<a href='terminarSesion'>Salir</a>");
+            out.println("</body>");
+            out.println("</html>");
+            out.close();
         }
-        out.println("<tr><td>Total general</td><td></td><td></td><td><center>$ "+ totalGeneral +"</center></td></tr>");
-        out.println("</table>");
-        out.println("<a href='productos'>Seguir comprando</a>");
-        out.println("<a href='terminarSesion'>Salir</a>");
-        out.println("</body>");
-        out.println("</html>");
-        out.close();
     }
 }
--- src/main/java/org/example/compras/ProductServlet.java
+++ src/main/java/org/example/compras/ProductServlet.java
@@ -31,34 +31,39 @@ public class ProductServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<style>table, th, td { border:1px solid black; min-width:200px; }</style>");
-        out.println("<head><title>Comprar golosinas</title></head>");
-        out.println("<body>");
-        out.println("<h1>Seleccione los productos</h1>");
-        out.println("<form action='facturar' method='GET'>");
-        out.println("<table style='width:30%'>");
-        out.println("<tr><th>Nombre</th><th>Precio unitario</th><th>Cantidad a comprar</th></tr>");
-        session = request.getSession();
-        Hashtable<String, Product> userProducts = (Hashtable<String, Product>) session.getAttribute("userProducts");
-        for (String i : products.keySet()) {
-            if (userProducts.containsKey(i)) {
-                products.get(i).setAmount(userProducts.get(i).getAmount());
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.sendRedirect("index.jsp");
+        } else {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<html>");
+            out.println("<style>table, th, td { border:1px solid black; min-width:200px; }</style>");
+            out.println("<head><title>Comprar golosinas</title></head>");
+            out.println("<body>");
+            out.println("<h1>Seleccione los productos</h1>");
+            out.println("<form action='facturar' method='GET'>");
+            out.println("<table style='width:30%'>");
+            out.println("<tr><th>Nombre</th><th>Precio unitario</th><th>Cantidad a comprar</th></tr>");
+            session = request.getSession();
+            Hashtable<String, Product> userProducts = (Hashtable<String, Product>) session.getAttribute("userProducts");
+            for (String i : products.keySet()) {
+                if (userProducts.containsKey(i)) {
+                    products.get(i).setAmount(userProducts.get(i).getAmount());
+                }
+                out.println("<tr>" +
+                        "<td>" + products.get(i).getName() + "</td>" +
+                        "<td><center>$ " + products.get(i).getPrice() + "</center></td>" +
+                        "<td><center><input type='number' name='" + i + "' value='" + products.get(i).getAmount() + "' /></center></td>" +
+                        "</tr>");
             }
-            out.println("<tr>" +
-                    "<td>" + products.get(i).getName() + "</td>" +
-                    "<td><center>$ " + products.get(i).getPrice() + "</center></td>" +
-                    "<td><center><input type='number' name='" + i + "' value='" + products.get(i).getAmount() + "' /></center></td>" +
-                    "</tr>");
+            out.println("</table>");
+            out.println("<input type='submit' value='Facturar' />");
+            out.println("</form>");
+            out.println("<a href='terminarSesion'>Salir</a>");
+            out.println("</body>");
+            out.println("</html>");
+            out.close();
         }
-        out.println("</table>");
-        out.println("<input type='submit' value='Facturar' />");
-        out.println("</form>");
-        out.println("<a href='terminarSesion'>Salir</a>");
-        out.println("</body>");
-        out.println("</html>");
-        out.close();
     }
 }
